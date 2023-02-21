5fbcc7ff9588d1b27db467efde5985f0749ea09f
saraduan <121904812+saraduan@users.noreply.github.com>
2023-02-20 22:48:17 -0800

    Add files via upload

---
+++ pressure_boxplots.R
@@ -0,0 +1,25 @@
+#  p2 boxplots
+# sara duan
+
+# load library
+library("dplyr")
+
+# load csv
+mars_df <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/mars-weather/mars-weather.csv", stringsAsFactors = FALSE)
+names(mars_df)
+sum(pressure)
+max(mars_df$ls, na.rm = TRUE)
+
+
+ls_range_df <- mars_df %>% 
+  group_by(ranges = cut(ls, breaks = seq(0, 360, by = 90)) ) %>% 
+  arrange(as.numeric(ranges))
+ls_range_df
+
+
+boxplot(pressure~ranges, data = ls_range_df, 
+        main = "Atmospheric Pressure in Mars for Each Season", 
+        at = c(1, 2, 3, 4),
+        names = c("Autumn", "Winter", "Spring", "Summer"), 
+        xlab = "Atmospheric Pressure", ylab = "Solar Longitude", col = "lightpink")
+
